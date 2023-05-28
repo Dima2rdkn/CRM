d3740b9637d945563c3cf6eadabd3fed20fa8ab0
Dmitri <dima2rdkn@yandex.ru>
2023-05-28 22:03:40 +0300

    Delete config.py
    Удалил API ключ

--- config.py
+++
@@ -1,6 +0,0 @@
-"""
-Файл конфигурации для хранения конфиденциальной информации,
-Не подлежит выгрузке на GIT. Обязательно прописать в gitignore
-"""
-
-DJANGO_SECRET_KEY = 'django-insecure-!fc$_ty8qrhb(pdj_3-zwkrvtm^to4y20*mur_hzn1v#u@3^m+'
